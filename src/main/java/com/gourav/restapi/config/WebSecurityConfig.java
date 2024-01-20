4ed88589fb5673d584cc4947631cae00037d0d6b
Artur Frimu <artur.frimu2@gmail.com>
2024-01-20 18:14:35 +0200

    feat: disable cors and csrf

--- src/main/java/com/gourav/restapi/config/WebSecurityConfig.java
+++ src/main/java/com/gourav/restapi/config/WebSecurityConfig.java
@@ -51,7 +51,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().and()
+        http
+                .cors().disable()
+                .csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
