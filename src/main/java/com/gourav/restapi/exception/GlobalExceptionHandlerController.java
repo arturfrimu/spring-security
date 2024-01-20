2e0755e4b9fe2da4f6fc48dd506808af58ed78da
Artur Frimu <artur.frimu2@gmail.com>
2024-01-20 18:13:32 +0200

    feat: add Global Exception Handler Controller

---
+++ src/main/java/com/gourav/restapi/exception/GlobalExceptionHandlerController.java
@@ -0,0 +1,26 @@
+package com.gourav.restapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandlerController {
+
+    @ResponseBody
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    String handleAllExceptions(Exception ex) {
+        return ex.getMessage();
+    }
+}
